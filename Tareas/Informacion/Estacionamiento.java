1af2e8c21fdcd4b2bfb89a96692dfa28e1c2e36f
PabloMazariegos-11 <bububu1611@gmail.com>
2026-08-14 22:29:39 -0600

    algoritmo de generacion aleatoria de Entrada y Salida en perimetro

--- Tareas/Informacion/Estacionamiento.java
+++ Tareas/Informacion/Estacionamiento.java
@@ -1,15 +1,21 @@
 package Tareas.Informacion;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Estacionamiento {
 
-    // Arreglo Nativo para la gestión interna de vehículos (8x8)
     private static String[][] parqueo = new String[8][8];
+
+    // Variables para Entrada y Salida
+    private static int entradaFila, entradaCol;
+    private static int salidaFila, salidaCol;
+
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         inicializarParqueo();
+        generarEntradaSalida();
 
         int opcion = 0;
         do {
@@ -38,13 +44,44 @@ public class Estacionamiento {
         } while (opcion != 7);
     }
 
-    // Inicializa todos los espacios como 'L' (Libre)
     private static void inicializarParqueo() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 parqueo[i][j] = "L";
             }
         }
+    }
+
+    private static void generarEntradaSalida() {
+        Random random = new Random();
+
+        do {
+            int[] pos = obtenerPosicionPerimetroAleatoria(random);
+            entradaFila = pos[0];
+            entradaCol = pos[1];
+        } while (esEsquina(entradaFila, entradaCol));
+
+        do {
+            int[] pos = obtenerPosicionPerimetroAleatoria(random);
+            salidaFila = pos[0];
+            salidaCol = pos[1];
+        } while ((salidaFila == entradaFila && salidaCol == entradaCol) || esEsquina(salidaFila, salidaCol));
+    }
+
+    private static int[] obtenerPosicionPerimetroAleatoria(Random r) {
+        int lado = r.nextInt(4);
+        int f = 0, c = 0;
+        switch (lado) {
+            case 0: f = 0; c = r.nextInt(10); break;
+            case 1: f = 9; c = r.nextInt(10); break;
+            case 2: f = r.nextInt(10); c = 0; break;
+            case 3: f = r.nextInt(10); c = 9; break;
+        }
+        return new int[]{f, c};
+    }
+
+    private static boolean esEsquina(int f, int c) {
+        return (f == 0 && c == 0) || (f == 0 && c == 9) || (f == 9 && c == 0) || (f == 9 && c == 9);
     }
 
     private static void mostrarMenu() {
